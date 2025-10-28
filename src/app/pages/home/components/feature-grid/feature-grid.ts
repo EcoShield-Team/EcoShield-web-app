cf7e95758a8451ab36f39eb6a546e226bf64ec7c
Gerardo Chavez <gerardomanuelrichard@gmail.com>
2025-10-28 04:12:51 -0500

    Fix: Feature routing on the home page

--- src/app/pages/home/components/feature-grid/feature-grid.ts
+++ src/app/pages/home/components/feature-grid/feature-grid.ts
@@ -1,19 +1,20 @@
 import {Component, signal} from '@angular/core';
 import {MATERIAL_IMPORTS} from '../../../../shared/material/material.imports';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-feature-grid',
-  imports: [MATERIAL_IMPORTS],
+  imports: [MATERIAL_IMPORTS, RouterLink],
   templateUrl: './feature-grid.html',
   styleUrl: './feature-grid.css',
 })
 export class FeatureGrid {
   featureCards = signal([
-    { title: 'Almanaque de enfermedades', description: 'Consulta el catálogo actualizado', icon: 'book_outline', color: 'color-green' },
-    { title: 'Detectar salud de cultivos', description: 'Analiza imágenes para evaluar vigor', icon: 'qr_code_scanner', color: 'color-blue' },
-    { title: 'Foro de comunidad', description: 'Comparte dudas y aprende', icon: 'forum', color: 'color-purple' },
-    { title: 'Recomendaciones de hoy', description: 'Acciones sugeridas según clima', icon: 'lightbulb', color: 'color-yellow' },
-    { title: 'Historial de fotos', description: 'Explora capturas anteriores', icon: 'history', color: 'color-indigo' },
-    { title: 'Enviar Feedback', description: 'Cuéntanos cómo mejorar EcoShield', icon: 'send', color: 'color-pink' },
+    { title: 'Almanaque de enfermedades', description: 'Consulta el catálogo actualizado', icon: 'book_outline', color: 'color-green', route: '/not-found' },
+    { title: 'Detectar salud de cultivos', description: 'Analiza imágenes para evaluar vigor', icon: 'qr_code_scanner', color: 'color-blue', route: '/home/deteccion' },
+    { title: 'Foro de comunidad', description: 'Comparte dudas y aprende', icon: 'forum', color: 'color-purple', route: '/not-found' },
+    { title: 'Recomendaciones de hoy', description: 'Acciones sugeridas según clima', icon: 'lightbulb', color: 'color-yellow', route: '/home/recomendaciones' },
+    { title: 'Historial de fotos', description: 'Explora capturas anteriores', icon: 'history', color: 'color-indigo', route: '/home/history' },
+    { title: 'Enviar Feedback', description: 'Cuéntanos cómo mejorar EcoShield', icon: 'send', color: 'color-pink', route: '/home/feedback' },
   ]);
 }
--- src/app/shared/components/header/header.ts
+++ src/app/shared/components/header/header.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MATERIAL_IMPORTS} from '../../material/material.imports';
 import {NgOptimizedImage} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MATERIAL_IMPORTS, NgOptimizedImage],
+  imports: [MATERIAL_IMPORTS, NgOptimizedImage, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
